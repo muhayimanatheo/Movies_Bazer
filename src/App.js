2ad811604c12d835a11aba8928cbe1c1ff667656
muhayimanatheo <muhayitheo23@gmail.com>
2024-03-27 10:23:28 +0200

    change

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@
 import './App.css';
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Layout from './components/layout';
+import Layout from './components/layout';
 import HomeFunc from './components/pages/home';
 import ExploreFunc from './components/pages/explore';
 import GenreFunc from './components/pages/genre';
 import NewsFunc from './components/pages/news';
 import ShowsFunc from './components/pages/tvshow';
 import MoviesFunc from './components/pages/movies';
-import NavBar from './components/navbar';
 
 function App() {
   return (
     <Router>
-      <NavBar>
       <Routes>
+        
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomeFunc />} />
+          {/*<Route path ="/details/:id" element={<Details/>}/>*/}
           <Route path="/explore/:id" element={<ExploreFunc />} />
           <Route path="/genre" element={<GenreFunc />} />
           <Route path="/news" element={<NewsFunc />} />
           <Route path="/movies/:id" element={<MoviesFunc />} />
           <Route path="/tvshow" element={<ShowsFunc />} />
+        </Route>
       </Routes>
-      </NavBar>
     </Router>
   );
 }
